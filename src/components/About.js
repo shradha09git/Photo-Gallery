b2464c1f4c83bfdee3fc8716de1f2b5a7bd89668
shradha09git <a.shradha26@gmail.com>
2022-12-23 22:08:26 +0530

    first

---
+++ src/components/About.js
@@ -0,0 +1,75 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import brandLogo from '../assets/brand-logo.png'
+
+const About = () => {
+  return (
+    <Container style={{ textAlign: "center" }}>
+      <h1 className="display-4 mt-2 mb-4">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa optio
+        magnam pariatur ducimus, reiciendis id, unde repellendus ea debitis,
+        minus sed. Autem, repudiandae. Aliquam aperiam ipsum cumque iste.
+        Consequuntur architecto enim accusantium, quae sed et tempora earum
+        sapiente necessitatibus nulla atque consequatur ratione itaque
+        voluptatibus sequi nostrum accusamus minus quia dolorem rem recusandae
+        ducimus est. Expedita repellendus illo ut quaerat culpa eveniet
+        repudiandae, atque recusandae maxime magni sit rerum velit ad! Quisquam
+        odio inventore minima ullam, expedita ea sunt ratione molestiae ex
+        magnam at magni, blanditiis, accusamus harum vitae debitis quaerat nemo!
+        Libero explicabo minus praesentium aliquid illum voluptas nesciunt,
+        commodi qui molestiae ipsa. Debitis laudantium iste odit eveniet vitae
+        assumenda molestiae quidem itaque accusamus minima autem doloremque
+        blanditiis ex quam, quibusdam corporis accusantium sapiente alias
+        incidunt quas. Temporibus voluptas necessitatibus neque animi omnis
+        earum sit in, aperiam quibusdam ipsam recusandae vel libero alias
+        laboriosam culpa porro dolores repellendus vitae corporis dolor possimus
+        ut! Ipsa eligendi, labore et esse laboriosam fuga nisi nemo veniam culpa
+        ab quidem deleniti quam odio dolor tenetur, dignissimos ea praesentium
+        consequuntur. Perferendis ducimus totam deleniti?
+      </p>
+      <hr />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil
+        voluptatibus facere aut illo omnis commodi quia corrupti dolor
+        laudantium eum rerum iure, repellendus cupiditate ut, quasi voluptatem
+        consequuntur quo similique.
+      </p>
+      <hr />
+      <Row>
+        <Col md={5}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          hic laboriosam suscipit labore in expedita alias. Distinctio quam fuga
+          perferendis recusandae natus amet provident incidunt fugit soluta
+          facere eos quis praesentium, illo officiis facilis sed dolorem,
+          impedit porro nisi eveniet? Illo qui iste quisquam nisi magni
+          molestiae. Dolor impedit obcaecati, amet voluptates quidem cupiditate
+          voluptatem aperiam ipsa deserunt? Ipsum reiciendis tempora,
+          reprehenderit ducimus rem temporibus a repudiandae. Ratione vero fugit
+          officia magni repellendus quis, fugiat vel ipsum pariatur, omnis
+          quidem.
+        </Col>
+        <Col md={2}>
+          <img src={brandLogo} alt="brand" style={{ height: "200px" }} />
+        </Col>
+				<Col md={5}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          hic laboriosam suscipit labore in expedita alias. Distinctio quam fuga
+          perferendis recusandae natus amet provident incidunt fugit soluta
+          facere eos quis praesentium, illo officiis facilis sed dolorem,
+          impedit porro nisi eveniet? Illo qui iste quisquam nisi magni
+          molestiae. Dolor impedit obcaecati, amet voluptates quidem cupiditate
+          voluptatem aperiam ipsa deserunt? Ipsum reiciendis tempora,
+          reprehenderit ducimus rem temporibus a repudiandae. Ratione vero fugit
+          officia magni repellendus quis, fugiat vel ipsum pariatur, omnis
+          quidem.
+        </Col>
+      </Row>
+			<hr />
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vero similique aliquid tenetur recusandae voluptates laborum quis inventore, fugit animi doloribus ipsam doloremque labore accusamus velit quaerat dicta fuga minus ullam possimus. Odio, nulla tenetur sint hic qui quis itaque quia minima iste modi architecto eveniet totam laudantium eius dolores soluta magnam numquam voluptatibus aliquid. Quos vero corrupti, id qui doloribus molestias incidunt vitae deleniti officia asperiores quidem aspernatur magni saepe pariatur dignissimos suscipit voluptate aut tempore sit, odio doloremque! Voluptatem libero consectetur fuga, obcaecati necessitatibus non porro rerum quia, explicabo quas labore, doloribus esse quis ab eos distinctio. Maiores provident repellat quasi! Id iste, porro minus voluptatem praesentium corrupti laudantium repellendus quae vitae ad ratione maxime sit harum aspernatur doloribus iusto consequatur fuga! Sequi repudiandae ut amet assumenda delectus dolorem, maiores ipsam cum suscipit deserunt officiis impedit laboriosam. Optio possimus fugit omnis est quis tenetur incidunt beatae id. Repudiandae ut voluptatum, eum, alias deserunt earum quas non sint repellat quod sit cumque enim molestiae perferendis culpa debitis fugiat numquam veritatis animi cupiditate ipsa architecto vitae! Facere repellat consequuntur quas enim, rem aliquam explicabo animi accusantium suscipit id, tenetur dolorem distinctio voluptas neque aperiam, nam vel delectus? Similique, quas assumenda.
+			</p>
+    </Container>
+  );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,42 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import brandLogo from '../assets/brand-logo.png'
+
+const Contact = () => {
+  return (
+    <Container style={{ textAlign: "center" }}>
+      <h1 className="display-4 mt-2 mb-4">Contact Us</h1>
+      <Row>
+        <Col md={10}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          hic laboriosam suscipit labore in expedita alias. Distinctio quam fuga
+          perferendis recusandae natus amet provident incidunt fugit soluta
+          facere eos quis praesentium, illo officiis facilis sed dolorem,
+          impedit porro nisi eveniet? Illo qui iste quisquam nisi magni
+          molestiae. Dolor impedit obcaecati, amet voluptates quidem cupiditate
+          voluptatem aperiam ipsa deserunt? Ipsum reiciendis tempora,
+          reprehenderit ducimus rem temporibus a repudiandae. Ratione vero fugit
+          officia magni repellendus quis, fugiat vel ipsum pariatur, omnis
+          quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          hic laboriosam suscipit labore in expedita alias. Distinctio quam fuga
+          perferendis recusandae natus amet provident incidunt fugit soluta
+          facere eos quis praesentium, illo officiis facilis sed dolorem,
+          impedit porro nisi eveniet? Illo qui iste quisquam nisi magni
+          molestiae. Dolor impedit obcaecati, amet voluptates quidem cupiditate
+          voluptatem aperiam ipsa deserunt? Ipsum reiciendis tempora,
+          reprehenderit ducimus rem temporibus a repudiandae. Ratione vero fugit
+          officia magni repellendus quis, fugiat vel ipsum pariatur, omnis
+          quidem.
+        </Col>
+        <Col md={2}>
+          <img src={brandLogo} alt="brand" style={{ height: "200px" }} />
+        </Col>
+      </Row>
+			<hr />
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vero similique aliquid tenetur recusandae voluptates laborum quis inventore, fugit animi doloribus ipsam doloremque labore accusamus velit quaerat dicta fuga minus ullam possimus. Odio, nulla tenetur sint hic qui quis itaque quia minima iste modi architecto eveniet totam laudantium eius dolores soluta magnam numquam voluptatibus aliquid. Quos vero corrupti, id qui doloribus molestias incidunt vitae deleniti officia asperiores quidem aspernatur magni saepe pariatur dignissimos suscipit voluptate aut tempore sit, odio doloremque! Voluptatem libero consectetur fuga, obcaecati necessitatibus non porro rerum quia, explicabo quas labore, doloribus esse quis ab eos distinctio. Maiores provident repellat quasi! Id iste, porro minus voluptatem praesentium corrupti laudantium repellendus quae vitae ad ratione maxime sit harum aspernatur doloribus iusto consequatur fuga! Sequi repudiandae ut amet assumenda delectus dolorem, maiores ipsam cum suscipit deserunt officiis impedit laboriosam. Optio possimus fugit omnis est quis tenetur incidunt beatae id. Repudiandae ut voluptatum, eum, alias deserunt earum quas non sint repellat quod sit cumque enim molestiae perferendis culpa debitis fugiat numquam veritatis animi cupiditate ipsa architecto vitae! Facere repellat consequuntur quas enim, rem aliquam explicabo animi accusantium suscipit id, tenetur dolorem distinctio voluptas neque aperiam, nam vel delectus? Similique, quas assumenda.
+			</p>
+    </Container>
+  );
+}
+
+export default Contact;
